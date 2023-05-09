b5b7b27df0172d8d83cf53ce5f466592691224d2
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-05-09 09:09:04 +0900

    initial commit

---
+++ pod/init.lua
@@ -0,0 +1,329 @@
+-- Parser of plain old documentation (POD) format.
+
+local M = {}
+
+local Innline = {
+  Q = function(source, i, j)
+    return source:sub(i, j)
+  end
+}
+
+local function guessNewline(source)
+  local i = 1
+  while i <= #source do
+    local c = source:sub(i, i)
+    if c == '\n' then
+      return '\n'
+    elseif c == '\r' then
+      if source:sub(i + 1, i + 1) == '\n' then
+        return '\r\n'
+      else
+        return '\r'
+      end
+    end
+    i = i + 1
+  end
+  return '\n'
+end
+
+local function splitLines(source, offset, limit)
+  offset = offset or 1
+  limit = limit or #source
+  local newline = guessNewline(source)
+  local lines = {}
+  local i = offset
+  for line in source:sub(offset, limit):gmatch("[^\r\n]*" .. newline) do
+    table.insert(lines, line)
+    i = i + #line
+  end
+  if i < limit then
+    table.insert(lines, source:sub(i))
+  end
+  return lines
+end
+
+local function offsetToRowCol(source, offset)
+  local row = 1
+  local col = 1
+  local i = 1
+  while i < offset do
+    local c = source:sub(i, i)
+    if c == '\n' then
+      row = row + 1
+      col = 1
+    elseif c == '\r' then
+      row = row + 1
+      col = 1
+      if source:sub(i + 1, i + 1) == '\n' then
+        i = i + 1
+      end
+    else
+      col = col + 1
+    end
+    i = i + 1
+  end
+  return row, col
+end
+
+local function findInline(source, offset, limit)
+  offset = offset or 1
+  limit = limit or #source
+  for b_cmd = offset, limit do
+    if source:sub(b_cmd, b_cmd):match("[A-Z]") then
+      if source:sub(b_cmd + 1, b_cmd + 1) == "<" then
+        local count = 1
+        local space = ""
+        local i = b_cmd + 2
+        local b_arg, e_arg = nil, nil
+        while i <= limit do
+          if source:sub(i, i) == "<" then
+            count = count + 1
+            i = i + 1
+          elseif source:sub(i, i):match("%s") then
+            b_arg = i + 1
+            space = "%s"
+            break
+          else
+            b_arg = b_cmd + 2
+            count = 1
+            break
+          end
+        end
+        if i > limit then
+          local row, col = offsetToRowCol(source, b_cmd)
+          error("Missing closing brackets '<" .. string.rep(">", count) ..
+                "':" .. row .. ":" .. col .. ": " .. source:sub(b_cmd, b_cmd + count))
+        end
+        local angles = space .. string.rep(">", count)
+        while i <= limit do
+          if source:sub(i, i + #angles - 1):match(angles) then
+            e_arg = i - 1
+            break
+          end
+          if source:sub(i, i) == "<" then
+            if source:sub(i - 1, i - 1):match("[A-Z]") then
+              _, _, _, i = findInline(source, i - 1)
+            end
+          end
+          i = i + 1
+        end
+        if i > limit then
+          local row, col = offsetToRowCol(source, b_cmd)
+          error("Missing closing brackets '" .. string.rep(">", count) ..
+                "':" .. row .. ":" .. col .. ": " .. source:sub(b_cmd, b_cmd + count))
+        end
+        return b_cmd, b_arg, e_arg, i + #angles - 1
+      end
+    end
+  end
+  return nil
+end
+
+local function splitParagraphs(source)
+  local state_list = 0
+  local state_para = 0
+  local state_verb = 0
+  local state_block = 0
+  local block_name = ""
+  local state_cmd = 0
+  local cmd_name = ""
+
+  local document = {}
+  local lines = {}
+  local nl = guessNewline(source)
+  for _, line in ipairs(splitLines(source .. nl .. nl)) do
+    if state_list > 0 then
+      table.insert(lines, line)
+      if line:match("^=over") then
+        state_list = state_list + 1
+      elseif line:match("^=back") then
+        state_list = state_list - 1
+      end
+      if state_list == 0 then
+        table.insert(document, { kind = "list", lines = lines })
+        lines = {}
+      end
+    elseif state_para > 0 then
+      table.insert(lines, line)
+      if state_para == 1 and line:match("^%s+$") then
+        table.insert(document, { kind = "para", lines = lines })
+        state_para = 0
+        lines = {}
+      end
+    elseif state_verb > 0 then
+      if state_verb == 1 and line:match("^%S") then
+        table.insert(document, { kind = "verb", lines = lines })
+        lines = {line}
+        state_verb = 0
+        if line:match("^=over") then
+          state_list = 1
+        elseif line:match("^=begin") then
+          state_block = 2
+          block_name = line:match("^=begin%s+(%S+)")
+        elseif line:match("^=") then
+          state_cmd = 1
+          cmd_name = line:match("^=(%S+)")
+        else
+          state_para = 1
+        end
+      else
+        table.insert(lines, line)
+        if line:match("^%s+$") then
+          state_verb = 1
+        end
+      end
+    elseif state_block > 0 then
+      table.insert(lines, line)
+      if line:match("^=end%s+" .. block_name) then
+        state_block = 1
+      end
+      if state_block == 1 and line:match("^%s+$") then
+        table.insert(document, { kind = block_name, lines = lines })
+        lines = {}
+        state_block = 0
+      end
+    elseif state_cmd > 0 then
+      table.insert(lines, line)
+      if state_cmd == 1 and line:match("^%s+$") then
+        table.insert(document, { kind = cmd_name, lines = lines })
+        lines = {}
+        state_cmd = 0
+      end
+    else
+      if line:match("^=over") then
+        table.insert(lines, line)
+        state_list = 1
+      elseif line:match("^=begin") then
+        table.insert(lines, line)
+        state_block = 2
+        block_name = line:match("^=begin%s+(%S+)")
+      elseif line:match("^[ \t]") then
+        table.insert(lines, line)
+        state_verb = 2
+      elseif line:match("^=") then
+        table.insert(lines, line)
+        state_cmd = 1
+        cmd_name = line:match("^=(%S+)")
+      else
+        table.insert(lines, line)
+        state_para = 1
+      end
+    end
+  end
+  offset = 1
+  for _, lines in ipairs(document) do
+    lines.offset = offset
+    for _, line in ipairs(lines.lines) do
+      offset = offset + #line
+    end
+    lines.limit = offset - 1
+  end
+  return document
+end
+
+local function splitParts(source, offset, limit)
+  local lines = {}
+  local state = 0
+  local parts = {}
+  for _, line in ipairs(splitLines(source, offset, limit)) do
+    if state == 0 then
+      if line:match("^=item") then
+        table.insert(lines, line)
+      elseif line:match("^=over") then
+        table.insert(parts, { kind = "part", lines = lines })
+        state = state + 1
+        lines = {}
+      else
+        table.insert(lines, line)
+      end
+    else
+      table.insert(lines, line)
+      if line:match("^=over") then
+        state = state + 1
+      elseif line:match("^=back") then
+        state = state - 1
+        if state == 0 then
+          table.insert(parts, { kind = "part", lines = lines })
+          lines = {}
+        end
+      end
+    end
+  end
+  if #lines > 0 then
+    table.insert(parts, { kind = "part", lines = lines })
+  end
+  offset = 1
+  for _, part in ipairs(parts) do
+    part.offset = offset
+    for _, line in ipairs(part.lines) do
+      offset = offset + #line
+    end
+    part.limit = offset - 1
+  end
+  return parts
+end
+
+local function splitItems(source, offset, limit)
+  offset = offset or 1
+  limit = limit or #source
+  local items = {}
+  local state = 0
+  local lines = {}
+  for _, line in ipairs(splitLines(source, offset, limit)) do
+    if state == 0 then
+      if line:match("^=item") then
+        table.insert(items, { kind = "over", lines = lines })
+        state = 1
+        lines = { line }
+      else
+        table.insert(lines, line)
+      end
+    elseif state >= 1 then
+      if line:match("^=item") then
+        table.insert(items, { kind = "item", lines = lines })
+        lines = { line }
+      elseif line:match("^=over") then
+        table.insert(lines, line)
+        state = state + 1
+      elseif line:match("^=back") then
+        table.insert(lines, line)
+        state = state - 1
+        if state == 0 then
+          lines[#lines] = nil
+          table.insert(items, { kind = "item", lines = lines })
+          lines = { line }
+        end
+      else
+        table.insert(lines, line)
+      end
+    end
+  end
+  table.insert(items, { kind = "back", lines = { lines } })
+  offset = 1
+  for _, item in ipairs(items) do
+    item.offset = offset
+    for _, line in ipairs(item.lines) do
+      offset = offset + #line
+    end
+    item.limit = offset - 1
+  end
+  return items
+end
+
+local function parse(source)
+  local document = splitParagraphs(source)
+  for _, paragraph in ipairs(document) do
+    if paragraph.kind == "para" then
+    elseif paragraph.kind == "list" then
+    elseif paragraph.kind == "verb" then
+    elseif paragraph.kind == "pod" then
+    elseif paragraph.kind == "cut" then
+    end
+  end
+end
+
+M.splitLines = splitLines
+M.splitParagraphs = splitParagraphs
+M.splitParts = splitParts
+M.splitItems = splitItems
+M.findInline = findInline
---
+++ pod_spec.lua
@@ -0,0 +1,1 @@
+local pod = require './pod'
