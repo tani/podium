0b0d4c08af135890b3bd3ec523631e834d77baad
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-06-09 21:39:46 +0900

    Use deno deploy

---
+++ app.ts
@@ -0,0 +1,28 @@
+import { Hono} from "https://lib.deno.dev/x/hono@v3/mod.ts";
+import { serveStatic } from "https://lib.deno.dev/x/hono@v3/middleware.ts";
+import { serve } from "https://deno.land/std@0.191.0/http/server.ts";
+import fengari from "https://esm.sh/fengari-web@0.1.4"
+
+let podium = await fetch(new URL('podium.lua', import.meta.url)).then(r => r.text());
+podium = podium.replace(/^#!.*\n/, '');
+
+function process(source: string, target: string): string {
+  const script = `
+    local SOURCE = [===[${source}]===]
+    local TARGET = [===[${target}]===]
+    ${podium}
+  `
+  return fengari.load(script)();
+}
+
+const app = new Hono();
+
+app.post("/:target{html|markdown|latex|vimdoc}", async (ctx) => {
+  const source = await ctx.req.text();
+  const target = ctx.req.param("target");
+  const result = process(source, target);
+  return ctx.text(result);
+})
+app.get("/*", serveStatic({ root: "." }));
+
+serve(app.fetch);
