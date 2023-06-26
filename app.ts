f4d893709c0f91df785ea5ac1ed4ac2a4f500f54
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-26 23:52:46 +0900

    Add lua directory

--- app.ts
+++ app.ts
@@ -3,14 +3,13 @@ import { serveStatic } from "https://lib.deno.dev/x/hono@v3/middleware.ts";
 import { serve } from "https://deno.land/std@0.191.0/http/server.ts";
 import fengari from "https://esm.sh/fengari-web@0.1.4"
 
-let podium = await fetch(new URL('podium.lua', import.meta.url)).then(r => r.text());
-podium = podium.replace(/^#!.*\n/, '');
+const podium = await fetch(new URL('lua/podium.lua', import.meta.url)).then(r => r.text());
 
 function process(source: string, target: string): string {
   const script = `
     local SOURCE = [===[${source}]===]
     local TARGET = [===[${target}]===]
-    ${podium}
+    ${podium.replace(/^#!.*\n/, '')}
   `
   return fengari.load(script)();
 }
@@ -22,6 +21,9 @@ app.post("/:target{html|markdown|latex|vimdoc}", async (ctx) => {
   const target = ctx.req.param("target");
   const result = process(source, target);
   return ctx.text(result);
+})
+app.get("/podium.lua", async (ctx) => {
+  return ctx.text(podium);
 })
 app.get("/*", serveStatic({ root: "." }));
 
