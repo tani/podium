6eb6133cd06621a95b7468915027b15ba0b68673
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-27 14:49:37 +0000

    Add deno directory

--- deno/app.ts
+++ deno/app.ts
@@ -3,7 +3,7 @@ import { serveStatic } from "https://lib.deno.dev/x/hono@v3/middleware.ts";
 import { serve } from "https://deno.land/std@0.191.0/http/server.ts";
 import fengari from "https://esm.sh/fengari-web@0.1.4"
 
-const podium = await fetch(new URL('lua/podium.lua', import.meta.url)).then(r => r.text());
+const podium = await fetch(new URL('podium.lua', import.meta.url)).then(r => r.text());
 
 function process(source: string, target: string): string {
   const script = `
@@ -21,9 +21,6 @@ app.post("/:target{html|markdown|latex|vimdoc}", async (ctx) => {
   const target = ctx.req.param("target");
   const result = process(source, target);
   return ctx.text(result);
-})
-app.get("/podium.lua", async (ctx) => {
-  return ctx.text(podium);
 })
 app.get("/*", serveStatic({ root: "." }));
 
