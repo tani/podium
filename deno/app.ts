3bb30994e5e4a25ac95f579ca76cf2d4c36cc6ed
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-28 04:50:27 +0000

    Remove symlink

--- deno/app.ts
+++ deno/app.ts
@@ -1,9 +1,9 @@
-import { Hono} from "https://lib.deno.dev/x/hono@v3/mod.ts";
+import { Hono } from "https://lib.deno.dev/x/hono@v3/mod.ts";
 import { serveStatic } from "https://lib.deno.dev/x/hono@v3/middleware.ts";
 import { serve } from "https://deno.land/std@0.191.0/http/server.ts";
 import fengari from "https://esm.sh/fengari-web@0.1.4"
 
-const podium = await fetch(new URL('podium.lua', import.meta.url)).then(r => r.text());
+const podium = await fetch(new URL('../lua/podium.lua', import.meta.url)).then(r => r.text());
 
 function process(source: string, target: string): string {
   const script = `
@@ -21,10 +21,14 @@ app.post("/:target{html|markdown|latex|vimdoc}", async (ctx) => {
   const target = ctx.req.param("target");
   const result = process(source, target);
   return ctx.text(result);
-})
+});
 
-const csd = new URL(import.meta.url).pathname.replace('/app.ts', '')
-const root = csd.replace(Deno.cwd(), '')
+app.get("/podium.lua", async(ctx) = > {
+  return ctx.text(podium);
+});
+
+const __dirname = new URL(import.meta.url).pathname.replace('/app.ts', '');
+const root = __dirname.replace(Deno.cwd(), '');
 app.get("/*", serveStatic({ root }));
 
 serve(app.fetch);
--- deno/podium.lua
+++
@@ -1,1 +0,0 @@
-../lua/podium.lua
