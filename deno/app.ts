16aa33a920c4cae0d9c7358a3cd78b58adef5738
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-06-28 07:20:48 +0000

    Format code

--- deno/app.ts
+++ deno/app.ts
@@ -1,16 +1,18 @@
 import { Hono } from "https://lib.deno.dev/x/hono@v3/mod.ts";
 import { serveStatic } from "https://lib.deno.dev/x/hono@v3/middleware.ts";
 import { serve } from "https://deno.land/std@0.191.0/http/server.ts";
-import fengari from "https://esm.sh/fengari-web@0.1.4"
+import fengari from "https://esm.sh/fengari-web@0.1.4";
 
-const podium = await fetch(new URL('../lua/podium.lua', import.meta.url)).then(r => r.text());
+const podium = await fetch(new URL("../lua/podium.lua", import.meta.url)).then(
+  (r) => r.text()
+);
 
 function process(source: string, target: string): string {
   const script = `
     _G.SOURCE = [===[${source}]===]
     _G.TARGET = [===[${target}]===]
-    ${podium.replace(/^#!.*\n/, '')}
-  `
+    ${podium.replace(/^#!.*\n/, "")}
+  `;
   return fengari.load(script)();
 }
 
@@ -23,12 +25,13 @@ app.post("/:target{html|markdown|latex|vimdoc}", async (ctx) => {
   return ctx.text(result);
 });
 
-app.get("/podium.lua", async (ctx) => {
+app.get("/podium.lua", (ctx) => {
   return ctx.text(podium);
 });
 
-const __dirname = new URL(import.meta.url).pathname.replace('/app.ts', '');
-const root = __dirname.replace(Deno.cwd(), '');
+const { pathname } = new URL(import.meta.url);
+const dirname = pathname.substring(0, pathname.lastIndexOf("/"));
+const root = dirname.replace(Deno.cwd(), "");
 app.get("/*", serveStatic({ root }));
 
 serve(app.fetch);
