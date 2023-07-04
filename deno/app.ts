b23d3a64d95328d7ef29b9b734ad59cc44ca163b
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-07-04 18:02:27 +0900

    Use bundle option

--- deno/app.ts
+++ deno/app.ts
@@ -2,7 +2,7 @@ import { Hono } from "https://lib.deno.dev/x/hono@v3/mod.ts";
 import { serveStatic } from "https://lib.deno.dev/x/hono@v3/middleware.ts";
 import { serve } from "https://deno.land/std@0.191.0/http/server.ts";
 import { createPodium, BackendName } from "./podium.ts";
-import { LuaFactory } from "https://esm.sh/wasmoon@1.15.0"
+import { LuaFactory } from "https://esm.sh/wasmoon@1.15.0?bundle"
 
 const luaFactory = new LuaFactory("https://esm.sh/wasmoon@1.15.0/dist/glue.wasm");
 const luaEngine = await luaFactory.createEngine();
