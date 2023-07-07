75d130b0ed153138b2533705a9cb5cd1d357e6a7
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-07-07 12:00:41 +0900

    This code change can be best described as a "refactor" because it involves modifying the code structure and organization without fixing a bug or adding a new feature.: refactor Lua scripting functionality
    - Change the `ensure` function signature to include a `lib` parameter
    - Replace the `fengari.lua` calls with `fengari[lib]` calls
    - Fetch and replace the `podium.lua` file contents in a more concise way
    - Add error checks for `lauxlib.luaL_dostring` and `lua.lua_getfield`
    - Push `backendName` and `source` literals onto the Lua stack
    - Add an error check for `lua.lua_pcall`
    - Retrieve the result from the Lua stack and assign it to `result`
    - Remove duplicate `lua.lua_close(L)` statement
    - Remove unnecessary Lua stack manipulation calls
    - Add a missing semicolon
    
    Signed-off-by: TANIGUHCI Masaya <ta2gch@gmail.com>

--- deno/podium.ts
+++ deno/podium.ts
@@ -1,7 +1,7 @@
 import fengari from "https://esm.sh/fengari@0.1.4"
 
-function ensure(type: string, fun: string, stack: unknown, ...args: unknown[]): void {
-  if (fengari.lua[fun](stack, ...args) !== fengari.lua[type]) {
+function ensure(type: string, lib: string, fun: string, stack: unknown, ...args: unknown[]): void {
+  if (fengari[lib][fun](stack, ...args) !== fengari.lua[type]) {
     fengari.lua.lua_close(stack);
     throw new Error(`${fun}(<stack>, ${args.join(", ")}) is not a ${type}`)
   }
@@ -13,23 +13,16 @@ export async function process(source: string, backendName: string): Promise<stri
   const lualib = fengari.lualib
   const L = lauxlib.luaL_newstate()
   lualib.luaL_openlibs(L)
-  const rawCode = await fetch(new URL("./podium.lua", import.meta.url)).then(r => r.text())
-  const luaCode = fengari.to_luastring(rawCode.replace("#!/usr/bin/env lua", ""));
-  if (lauxlib.luaL_dostring(L, luaCode) !== lua.LUA_OK) {
-    lua.lua_close(L);
-    throw new Error(lua.lua_tojsstring(L, -1))
-  }
-  ensure("LUA_TTABLE", "lua_getfield", L, -1, "PodiumProcessor")
-  ensure("LUA_TFUNCTION", "lua_getfield", L, -1, "new")
-  ensure("LUA_TTABLE", "lua_getfield", L, -3, backendName)
-  ensure("LUA_OK", "lua_pcall", L, 1, 1, 0)
-  lua.lua_setglobal(L, "processor")
-  ensure("LUA_TTABLE", "lua_getglobal", L, "processor")
-  ensure("LUA_TFUNCTION", "lua_getfield", L, -1, "process")
-  ensure("LUA_TTABLE", "lua_getglobal", L, "processor")
-  lua.lua_pushliteral(L, source)
-  ensure("LUA_OK", "lua_pcall", L, 2, 1, 0)
-  const result = lua.lua_tojsstring(L, -1)
+  const rawCode = await fetch(new URL("./podium.lua", import.meta.url))
+    .then(r => r.text())
+    .then(r => r.replace("#!/usr/bin/env lua", ""))
+  const luaCode = fengari.to_luastring(rawCode);
+  ensure("LUA_OK", "lauxlib", "luaL_dostring", L, luaCode);
+  ensure("LUA_TFUNCTION", "lua", "lua_getfield", L, -1, "process");
+  lua.lua_pushliteral(L, backendName);
+  lua.lua_pushliteral(L, source);
+  ensure("LUA_OK", "lua", "lua_pcall", L, 2, 1, 0);
+  const result = lua.lua_tojsstring(L, -1);
   lua.lua_close(L);
-  return result
+  return result;
 }
